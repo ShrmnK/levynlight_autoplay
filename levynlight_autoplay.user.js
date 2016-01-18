e28edf14f8f1188e5b9f2a2eb1d00ea2c0f32a22
ShrmnK <github@shrmn.com>
2016-01-18 10:44:36 +0800

    Code optimizations

--- levynlight_autoplay.user.js
+++ levynlight_autoplay.user.js
@@ -3,7 +3,7 @@
 // @namespace      http://www.shrmn.com/
 // @description    Automatically plays LevynLight turn, shows time to next turn in title bar and sub-menu bar.
 // @copyright      2010, Shrmn K (http://www.shrmn.com/)
-// @version        0.1.1
+// @version        0.1.2
 // @include        http://apps.facebook.com/levynlight/*
 // @include        http://apps.new.facebook.com/levynlight/*
 // @require        http://getelementsbyclassname.googlecode.com/files/getElementsByClassName-1.0.1.js
@@ -14,32 +14,42 @@ var subMenu = document.getElementById("app377144924760_subMenu");
 var subMenuHTML = subMenu.innerHTML;
 var battleInProgress = false;
 
+function updateStatus(text) {
+	// For title, strip tags of text.
+	document.title = text.replace(/(<([^>]+)>)/ig,"") + " | " + pageTitle;
+	subMenu.innerHTML = '<span style="border: 1px solid; padding: 1px 1px 1px 1px; color: #999999; left: 215px; position: absolute; top: 13px;">&nbsp;<b>[<a href="http://userscripts.org/scripts/show/80811" target="_blank" style="color: #666666; text-decoration: none;">AutoPlay</a> v' + _LLAPversion + ']</b> ' + text + '&nbsp;</span>&nbsp;' + subMenuHTML;
+}
+
 function checkActions() {
+	// Check if there is energy. Will terminate everything if there is no energy.
+	if(document.getElementById('app377144924760_hud_energy_quantity').innerHTML == 0) {
+		updateStatus('Out of Energy!');
+		return;
+	}
+	// Terminate this loop if there is a battle in progress. Battle Loop will take over.
+	if(battleInProgress)	return;
+	
 	var actions = document.getElementById('app377144924760_hud_actions').innerHTML;
 	if(actions > 0) {
-		// Play turn (wait 3 seconds to load)
-		setTimeout(playTurn, 3000);
-		// Change window title
-		document.title = "Playing turn... | " + pageTitle;
+		// Play turn (wait 1 second + random time between 1 and 5 seconds)
+		setTimeout(playTurn, 1000 + Math.floor(Math.random()*5+1)*1000);
+		updateStatus('Playing turn...');
 		//window.getAttention();
 	} else {
-		if(!battleInProgress) {
-			var timeleft = document.getElementById('app377144924760_playCounter').innerHTML;
-			timeleft = timeleft.replace('+1 action in: ', '');
-			var splitTime = timeleft.split(':');
-			// Calculate time left in seconds, and add random number between 0 to 10
-			//var secondsToPlay = parseInt(splitTime[0])*60 + parseInt(splitTime[1]) + Math.floor(Math.random()*10+1);
-			var secondsToPlay = parseInt(splitTime[0])*60 + parseInt(splitTime[1]);
-			if(splitTime.length == 2 && typeof(secondsToPlay) == 'number') {
-				// Check every 5 seconds
-				setTimeout(checkActions, 5000);
-				//alert("TIMEOUT SET: " + timeleft + " (" + splitTime[0] + "/" + splitTime[1] + ") -- " + secondsToPlay + "s");
-				subMenu.innerHTML = '<span style="border: 1px solid; padding: 1px 1px 1px 1px; color: #999999; left: 215px; position: absolute; top: 13px;">&nbsp;<b>[AutoPlay]</b> Time to next Turn: <b>' + secondsToPlay + "s</b>&nbsp;</span>&nbsp;" + subMenuHTML;
-				document.title = "Next Turn in " + secondsToPlay + "s | " + pageTitle;
-			} else {
-				setTimeout(checkActions, 1000);
-				//alert("Page not fully loaded yet, check again in 1 second");
-			}
+		var timeleft = document.getElementById('app377144924760_playCounter').innerHTML;
+		timeleft = timeleft.replace('+1 action in: ', '');
+		var splitTime = timeleft.split(':');
+		// Calculate time left in seconds, and add random number between 0 to 10
+		//var secondsToPlay = parseInt(splitTime[0])*60 + parseInt(splitTime[1]) + Math.floor(Math.random()*10+1);
+		var secondsToPlay = parseInt(splitTime[0])*60 + parseInt(splitTime[1]);
+		if(splitTime.length == 2 && typeof(secondsToPlay) == 'number') {
+			// Check every 5 seconds
+			setTimeout(checkActions, 5000);
+			//alert("TIMEOUT SET: " + timeleft + " (" + splitTime[0] + "/" + splitTime[1] + ") -- " + secondsToPlay + "s");
+			updateStatus('Next Turn in: <b>' + secondsToPlay + 's</b>');
+		} else {
+			setTimeout(checkActions, 1000);
+			//alert("Page not fully loaded yet, check again in 1 second");
 		}
 	}
 }
@@ -56,12 +66,12 @@ function playTurn() {
 		
 		battleInProgress = true;
 		// Run loop to check for battle status after 3 seconds (enough for ajax to initiate)
-		setTimeout(superBattleLoop, 3000);
+		setTimeout(loopWhileBattle, 3000);
 	}
 }
 
-function superBattleLoop() {
-	document.title = "Battle ongoing... | " + pageTitle;
+function loopWhileBattle() {
+	updateStatus("Battle ongoing...");
 	var winnerDiv = getElementsByClassName("winner player", "div", document.getElementById('app377144924760_turnSummary'));
 	var roundOver = getElementsByClassName("hidden player", "div", document.getElementById('app377144924760_turnStack'));
 	
@@ -74,18 +84,19 @@ function superBattleLoop() {
 	
 	if(roundOver.length == 1 && roundOver[0].getAttribute("id") == "app377144924760_turnSummary") {
 		// Run this every second until the battle is over.
-		setTimeout(superBattleLoop, 1000);
+		setTimeout(loopWhileBattle, 1000);
 	} else {
 		battleInProgress = false;
 		var loot = getElementsByClassName("lootContent clear-block", "div", document.getElementById('app377144924760_turnSummary'));
 		if(loot.length == 0)
-			document.title = won + " | Round ended | " + pageTitle;
+			updateStatus(won);
 		else
-			document.title = won + " (Loot Present) | Round ended | " + pageTitle;
+			updateStatus(won + " (Loot Present)");
 		// Refresh page
 		window.location = "http://apps.facebook.com/levynlight/";
 	}
 }
 
-// Activates script 1 second after page loads.
-setTimeout(checkActions, 1000);
+if (document.addEventListener)
+	document.addEventListener("DOMContentLoaded", checkActions, false);
+var _LLAPversion = '0.1.2';
